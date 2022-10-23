141fcafe47b8f6259f5a8269d5d6b0a1d23cc859
MrNihalX <109473632+MrNihalX@users.noreply.github.com>
2022-10-23 16:52:25 +0530

    Update start.py

--- FallenMusic/Helpers/Inline/start.py
+++ FallenMusic/Helpers/Inline/start.py
@@ -30,7 +30,7 @@ def start_pannel():
             ],
             [
                 InlineKeyboardButton(
-                    text="☁ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ☁", url="https://github.com/AnonymousR1025/FallenMusic"
+                    text="☁ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ☁", url="https://t.me/Mr_Nihal9"
                 )
             ],
         ]
@@ -62,7 +62,7 @@ def private_panel():
             ],
             [
                 InlineKeyboardButton(
-                    text="☁ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ☁", url="https://github.com/AnonymousR1025/FallenMusic"
+                    text="☁ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ☁", url="https://t.me/Mr_Nihal9"
                 ),
             ],
         ]
